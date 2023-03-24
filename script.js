ecf3b5181b6cf8cf9911ec90a48d8d2c2dd8fbe7
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-03-23 21:09:29 -0400

    working on js file

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const currentTime = document.querySelector(".currentTime");
+const currentTime = document.querySelector(".current-time");
+const nameInput = document.querySelector(".input-name");
+const idInput = document.querySelector(".input-id");
+const clockInBtn = document.querySelector(".clock-in-btn");
+
+class TimeStamp {
+  constructor(name, id, time, date) {
+    this.name = name;
+    this.id = id;
+    this.time = time;
+    this.date = date;
+  }
+}
 
 class App {
+  username;
+  idNumber;
+  time;
   constructor() {
-    console.log(this.displayTime);
+    setInterval(this.displayTime, 1000);
+    nameInput.addEventListener("change", this.inputData.bind(this));
+    idInput.addEventListener("change", this.inputData.bind(this));
   }
+
+  inputData() {
+    this.username = nameInput.value;
+    this.idNumber = idInput.value;
+    if (this.username) {
+      clockInBtn.classList.add("clock-in-true");
+    }
+  }
+
   displayTime() {
     let date = new Date();
     let hour = date.getHours();
@@ -17,10 +43,12 @@ class App {
       hour = hour - 12;
       session = "PM";
     }
+
     hour = hour < 10 ? "0" + hour : hour;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     let time = `${hour}:${minutes}:${seconds} ${session}`;
+    currentTime.innerHTML = time;
   }
 }
 
