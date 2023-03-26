84f025f97c0a02bb14ed59a202241c9696d2849f
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-03-25 21:22:19 -0400

    Hard time building chronomter

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const currentTime = document.querySelector(".current-time");
+const displayTime = document.querySelector(".current-time");
 const nameInput = document.querySelector(".input-name");
 const idInput = document.querySelector(".input-id");
 const clockInBtn = document.querySelector(".clock-in-btn");
+const displayUsername = document.querySelector(".display-user-name");
 
 class TimeStamp {
   constructor(name, id, time, date) {
@@ -15,40 +16,78 @@ class TimeStamp {
 class App {
   username;
   idNumber;
+  clockInTime;
+  clockOutTime;
+  date;
   time;
+  minutes = 0;
+  seconds = 0;
   constructor() {
     setInterval(this.displayTime, 1000);
     nameInput.addEventListener("change", this.inputData.bind(this));
     idInput.addEventListener("change", this.inputData.bind(this));
+    clockInBtn.addEventListener("click", this.btnFunction.bind(this));
   }
 
   inputData() {
     this.username = nameInput.value;
     this.idNumber = idInput.value;
-    if (this.username) {
-      clockInBtn.classList.add("clock-in-true");
-    }
+    this.username
+      ? clockInBtn.classList.add("clock-in-true")
+      : clockInBtn.classList.remove("clock-in-true");
+  }
+
+  workingTime() {
+    this.seconds++;
+    console.log(this.seconds);
+  }
+
+  hideInputs() {
+    nameInput.style.display = "none";
+    idInput.style.display = "none";
+  }
+
+  unHideInputs() {
+    nameInput.style.display = "block";
+    idInput.style.display = "block";
   }
 
   displayTime() {
-    let date = new Date();
-    let hour = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let session = "AM";
-    if (hour == 0) {
-      hour = 12;
-    }
-    if (hour > 12) {
-      hour = hour - 12;
-      session = "PM";
-    }
+    this.date = new Date();
+    this.time = this.date.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    displayTime.innerHTML = this.time;
+    return this.date;
+  }
 
-    hour = hour < 10 ? "0" + hour : hour;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    let time = `${hour}:${minutes}:${seconds} ${session}`;
-    currentTime.innerHTML = time;
+  btnFunction() {
+    if (clockInBtn.getAttribute("state") === "false") {
+      this.clockIn();
+    } else {
+      this.clockOut();
+    }
+  }
+
+  clockIn() {
+    this.hideInputs();
+    this.inputData();
+    displayUsername.innerHTML = `Hello, ${this.username}`;
+    clockInBtn.classList.remove("clock-in-true");
+    clockInBtn.classList.add("clock-in-false");
+    clockInBtn.innerHTML = "Clock Out";
+    clockInBtn.setAttribute("state", "true");
+  }
+
+  clockOut() {
+    this.unHideInputs();
+    displayUsername.innerHTML = "";
+    this.clockOutTime = this.displayTime();
+    clockInBtn.innerHTML = "Clock In";
+    clockInBtn.classList.remove("clock-in-false");
+    clockInBtn.setAttribute("state", "false");
   }
 }
 
